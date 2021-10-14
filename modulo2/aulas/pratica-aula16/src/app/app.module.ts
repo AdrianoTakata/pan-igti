6eae0614e06f6fc5e3d93fef9261c174895f17a5
AdrianoTakata <adrianostakata@gmail.com>
2021-10-14 12:07:30 -0300

    update

---
+++ modulo2/aulas/pratica-aula16/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule} from '@angular/material/table';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatTableModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
